450e4f0364b69d9c82c4643c297d1969acdd1a5e
Sunny Moore <moore.sunny@gmail.com>
2022-09-25 15:35:32 -0700

    create geocode service and tests.  add fixtures for vcr cassettes

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,12 +1,17 @@
 class GeocodeService
 
   def self.geocode(location)
-    response = conn.get("?key=#{ENV['mq_api_key']}&location=#{location}")
+    response = conn.get("/geocoding/v1/address?location=#{location}")
+    parse_json(response)[:results][0][:locations][0][:latLng]
+  end
+
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn
-    Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/address') do |faraday|
-      faraday.params['api_key'] = ENV.fetch('mq_api_key', nil)
+    Faraday.new(url: 'http://www.mapquestapi.com') do |faraday|
+      faraday.params['key'] = ENV.fetch('mq_api_key', nil)
     end
   end
 end
--- spec/services/geocode_spec.rb
+++ spec/services/geocode_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe GeocodeService do
-  it 'returns latitude/longitude for a given city', vcr: 'geocode' do
+RSpec.describe 'GeocodeService' do
+  it 'returns latitude/longitude for a given city', :vcr do
     location = 'portland, or'
+    VCR.use_cassette('geocode') do
+      @results = GeocodeService.geocode(location)
+    end
 
-    results = GeocodeService.geocode(location)
-
-    expect(results).to be_a Hash
-    expect(results[0]).to be_a Array
-    expect(results[0][:providedLocation][:location]).to eq(location)
-    expect(results[0][:locations][:latLng]).to have_key(:lat)
-    expect(results[0][:locations][:latLng][:lat]).to eq(45.516018)
-    expect(results[0][:locations][:latLng]).to have_key(:lng)
-    expect(results[0][:locations][:latLng][:lng]).to eq(-122.681425)
+    expect(@results).to be_a Hash
+    expect(@results).to have_key(:lat)
+    expect(@results[:lat]).to eq(45.516018)
+    expect(@results).to have_key(:lng)
+    expect(@results[:lng]).to eq(-122.681425)
   end
 end
